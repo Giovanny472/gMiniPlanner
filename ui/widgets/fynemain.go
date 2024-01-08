18f247e637e4c5f058a9f004daea5d8eddae261a
Giovanny472 <egmn472@yandex.com>
2024-01-08 09:42:57 +0300

    add layouts

--- ui/widgets/fynemain.go
+++ ui/widgets/fynemain.go
@@ -3,12 +3,18 @@ package widgets
 import (
 	"fyne.io/fyne/v2"
 	"fyne.io/fyne/v2/app"
+	"fyne.io/fyne/v2/container"
+	"fyne.io/fyne/v2/widget"
 
 	"github.com/Giovanny472/gMiniPlanner/interfaces"
 	"github.com/Giovanny472/gMiniPlanner/ui"
 )
 
-type fynemain struct{}
+type fynemain struct {
+	menu      *fyne.Container
+	detail    *fyne.Container
+	listPlans *widget.List
+}
 
 var fm *fynemain
 
@@ -24,9 +30,17 @@ func (f *fynemain) Show() {
 
 	app := app.NewWithID("io.ve.planner")
 	win := app.NewWindow("Мини-План")
-
 	win.Resize(fyne.NewSize(ui.Width, ui.Height))
-	win.Show()
 
+	// layouts
+	f.menu = container.NewVBox()
+	f.detail = container.NewStack()
+
+	//split
+	split := container.NewHSplit(f.menu, f.detail)
+	split.Offset = 0.2
+	win.SetContent(split)
+
+	win.Show()
 	app.Run()
 }
